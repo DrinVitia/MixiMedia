d4019d935aef50e8112d7a94123167799e0dacdb
Drin Vitia <117443499+DrinVitia@users.noreply.github.com>
2024-06-11 17:52:08 +0200

    chore: Update About page UI and image source

--- MixiMedia/src/Pages/About.jsx
+++ MixiMedia/src/Pages/About.jsx
@@ -7,18 +7,18 @@ const About = () => {
       <div className="absolute inset-0 w-full h-full top-0 left-0">
         <ParticleBackground />
       </div>
-      <div className="absolute self-center left-[25vw] flex flex-col items-center justify-center min-h-screen bg-transparent text-white p-8 overflow-hidden">
-        <div className="flex flex-col items-center space-y-8 bg-gray-900 p-24">
+      <div className="absolute self-center left-[25vw] flex flex-col items-center justify-center min-h-screen bg-transparent text-stone-100 p-8 overflow-hidden">
+        <div className="flex flex-col items-center space-y-8 bg-stone-800 p-24">
           <h1 className="text-4xl font-bold">About MixiMedia</h1>
           <p className="text-center">
             MixiMedia is a music player app built with React and Tailwind CSS.
             It uses the Web Audio to play audio files.
           </p>
           <img
-            src="https://magnolia.fillonit.tech/assets/Drugs-BkebkmFX.jpg"
+            src="/images/asap.jpg"
             width={400}
             height={400}
-            alt="Test"
+            alt="ASAP ROCKY"
           />
         </div>
       </div>
--- MixiMedia/src/components/Navbar.jsx
+++ MixiMedia/src/components/Navbar.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { FaPlay } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="bg-stone-300 shadow fixed top-0 w-full z-50">
+    <header className="bg-stone-200 shadow fixed top-0 w-full z-50">
       <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
         <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-black lg:items-center lg:justify-center mb-4 md:mb-0">
-          <FaPlay className="w-8 h-8 text-stone-700 p-2 bg-stone-500 rounded-full" />
-          <span className="ml-3 text-xl">MixiMedia</span>
+          <Link to="/">
+            <FaPlay className="w-8 h-8 text-stone-700 p-2 bg-stone-500 rounded-full" />
+          </Link>
+          <Link to="/" className="ml-3 text-xl">
+            MixiMedia
+          </Link>
         </a>
+        <nav className="lg:w-2/5 flex flex-wrap items-center text-base md:ml-2">
+          <Link
+            to="/"
+            className="mr-5 hover:text-gray-900 text-lg font-semibold"
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            className="mr-5 hover:text-gray-900 text-lg font-semibold"
+          >
+            About
+          </Link>
+        </nav>
       </div>
     </header>
   );
